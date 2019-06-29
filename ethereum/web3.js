269d002b1211a7140f3692f324663ea9d0a2778f
Casey Ross <caseyross003@gmail.com>
2019-06-29 16:05:45 -0500

    refactor campaigns call to next server

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -1,5 +1,5 @@
 import Web3 from 'web3';
-import rinkebyURL from './rinkeby';
+const { rinkebyURL } = require('./rinkeby');
 
 let web3;
 
--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import factory from '../ethereum/factory';
 
 class CampaignHome extends Component {
-    async componentDidMount() {
+    static async getInitialProps() {
         const campaigns = await factory.methods.getDeployedCampaigns().call();
-        console.log(campaigns);
+        return { campaigns };
     }
 
     render() {
-        return(<h1>Campaigns</h1>);
+        return(<h1>{this.props.campaigns[0]}</h1>);
     }
 }
 
