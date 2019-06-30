c617c9ffdb85fadc557d7ba0b31bff7a8146a4a7
Casey Ross <caseyross003@gmail.com>
2019-06-30 16:41:30 -0500

    add submit to create new campaign

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,19 +1,38 @@
 import React, { Component } from 'react';
 import factory from '../../ethereum/factory';
-import { Form, Checkbox, Button, Input } from 'semantic-ui-react';
+import web3 from '../../ethereum/web3';
+import { Form, Checkbox, Button, Input, Message } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
 
 class CampaignNew extends Component {
 
     state = {
-        minimumContribution: ''
+        minimumContribution: '',
+        errorMessage: ''
+    };
+
+    onSubmit = async () => {
+        event.preventDefault();
+
+        try{
+            const accounts = await web3.eth.getAccounts();
+
+            await factory.methods
+                .createCampaign(this.state.minimumContribution)
+                .send({
+                    from: accounts[0]
+                });
+        } catch (err) {
+            this.setState({ errorMessage: err.message });
+        }
+
     };
 
     render() {
         return(
             <Layout>
                     <h3>Create a Campaign</h3>
-                  <Form>
+                  <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage} >
                     <Form.Field>
                         <label>Minimum Contribution</label>
                         <Input 
@@ -25,6 +44,7 @@ class CampaignNew extends Component {
                     <Form.Field>
                         <Checkbox label='I agree to the Terms and Conditions' />
                     </Form.Field>
+                    <Message error header="Oops! A new campaign could not be created." content={this.state.errorMessage} />
                     <Button type='submit' primary>Create</Button>
                 </Form>
             </Layout>
